6dbda09a57eacaece85fe7c79b73fcbbb39173e4
yangw0069 <YangW0069@163.com>
2019-04-25 15:26:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/pet/message_wdy_activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.pet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class message_wdy_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message_wdy_activity);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/pet/publish_wdy_activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.pet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class publish_wdy_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.publish_wdy_activity);
+    }
+}
